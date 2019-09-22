f17bd003bcfe8eb9dc8d111063c06d28e513ddbd
watsjn <aliaksei.shumovich@gmail.com>
2019-09-22 14:33:46 +0200

    revert config

--- src/environments/development.tsx
+++
@@ -1,8 +0,0 @@
-import environment from './base';
-const baseApi = 'http://localhost:3000';
-const env = environment(baseApi);
-export default {
-  ...env,
-  isProduction: false,
-  isDevelopment: true,
-};
--- src/environments/production.tsx
+++
@@ -1,6 +0,0 @@
-import environment from './base';
-const baseApi = 'http://www.production-api.com';
-const env = environment(baseApi);
-export default {
-  ...env,
-};
